afa8159ce99feaee89afb021d498d3684593eed6
Viduranga Wijesooriya <vpowerrc@gmail.com>
2017-01-13 09:54:18 +0530

    set logger

--- lib/notification_hub.rb
+++ lib/notification_hub.rb
@@ -5,11 +5,13 @@ require "httparty"
 module NotificationHub  
   mattr_accessor :user_class
   mattr_accessor :events
-  @@user_class = "user"
+  mattr_accessor :logger     
 
-  class << self  	
-	
+  class << self    
 		def setup
+      @@user_class = "User"
+      @@logger = Rails.logger
+
       raise ArgumentError, 'No block given for setup' unless block_given?
       yield self   
     end
@@ -30,15 +32,18 @@ module NotificationHub
     	NotificationHubJob.perform_later(user_id, event_id, data, options)
     end
 
-    def send_message(user_id, event_id, data, options = nil)    	
-    	#query subsccriptions for the relevant event    	
+    def send_message(user_id, event_id, data, options = nil)  
+    	# Query subsccriptions for the relevant event    	
       susbcriptions = NotificationHub::Subscription.where("#{user_model}_id" => user_id)
 
-      options = {
-        webhook_url: "https://httpbin.org/post",
-        phone_number: "+94716513320",
-        token: "dfSfHOsxo08:APA91bG25YM94QkkTsnBihEAg0VlocA7KRQuWo4P6Hyxn-_rPB0lpWqLkf4AIbcIuPxeGMh_5tWCYGjdfV98raXE8_APWvmuPpk-8t5SUEoRXMuPpZVvpuxpdTkrY2LR31zQdBzKkZsO"
-      }
+      # Example device details
+      # device_details = {
+      #   email: "vpowerrc@gmail.com",
+      #   webhook_url: "https://httpbin.org/post",
+      #   phone_number: "+94716513320",
+      #   push_token: "dfSfHOsxo08:APA91bG25YM94QkkTsnBihEAg0VlocA7KRQuWo4P6Hyxn-_rPB0lpWqLkf4AIbcIuPxeGMh_5tWCYGjdfV98raXE8_APWvmuPpk-8t5SUEoRXMuPpZVvpuxpdTkrY2LR31zQdBzKkZsO",
+      #   push_platform: "android"
+      # }
 
       susbcriptions.each do |susbcription|
         if susbcription.gateway_id.present?
@@ -46,7 +51,15 @@ module NotificationHub
         else
           channel_const = "NotificationHub::Channels::#{susbcription.channel_id.camelize}".constantize
         end
-        channel_const.send_message(event_id, data, options)
+
+        susbcription.notification_hub_devices.try(:each) do |device|
+          device_details = device.attributes.symbolize_keys
+          begin
+            channel_const.send_message(event_id, data, device_details)
+          rescue => e
+            NotificationHub.logger.error e.message
+          end          
+        end
       end     
     end
 
